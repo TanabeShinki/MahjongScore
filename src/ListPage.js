d28ed4c12cd4b88a2d9f0ba218c13de7836d2688
xx_tnb_xx <tana88shin@gmail.com>
2023-03-28 20:28:19 +0900

    ListQueryDeploy

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { API } from "aws-amplify";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Row, Col, Form } from 'react-bootstrap';
-import { listAccounts } from "./graphql/queries";
+import { Button, Container, Row, Col, Form, Table } from 'react-bootstrap';
+import { listScores, listAccounts } from "./graphql/queries";
 import { useNavigate } from "react-router-dom";
 
 function ListPage() {
   const [accountList, setAccountList] = useState([]);
   const [selectedAccount, setSelectedAccount] = useState('');
+  const [scoreList, setScoreList] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,10 +24,43 @@ function ListPage() {
     }
   }
 
-  const handleSelectChange = (event) => {
+  const handleSelectChange = async (event) => {
     setSelectedAccount(event.target.value);
+    try {
+      const apiData = await API.graphql({
+        query: listScores,
+        variables: {
+          filter: {
+            or: [
+              { Player1ID: { eq: event.target.value } },
+              { Player2ID: { eq: event.target.value } },
+              { Player3ID: { eq: event.target.value } },
+              { Player4ID: { eq: event.target.value } }
+            ]
+          }
+        }
+      });
+      alert(event.target.value)
+      setScoreList(apiData.data.listScores.items);
+    } catch (error) {
+      console.log("error fetching scores", error);
+    }
   }
 
+  const getAccountNameById = (id) => {
+    const account = accountList.find((account) => account.id === id);
+    return account ? account.name : "";
+  };
+
+  const scoreListByDate = {};
+  if (scoreList !== null) {
+    scoreList.forEach((score) => {
+      if (!scoreListByDate[score.MatchDate]) {
+        scoreListByDate[score.MatchDate] = [];
+      }
+      scoreListByDate[score.MatchDate].push(score);
+    });
+  }
   return (
     <Container>
       <Row>
@@ -47,6 +81,38 @@ function ListPage() {
             <div className="text-center my-3">
               <Button variant="info" onClick={() => navigate('/')}>入力画面に戻ルよ</Button>
             </div>
+            {selectedAccount && (
+              <div>
+                {Object.keys(scoreListByDate).map((date, index) => (
+                  <div key={index}>
+                    <h2>{date}</h2>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>{getAccountNameById(scoreListByDate[date][0]?.Player1ID)}</th>
+                          <th>{getAccountNameById(scoreListByDate[date][0]?.Player2ID)}</th>
+                          <th>{getAccountNameById(scoreListByDate[date][0]?.Player3ID)}</th>
+                          <th>{getAccountNameById(scoreListByDate[date][0]?.Player4ID)}</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {scoreListByDate[date].map((score) => (
+                          <tr key={score.id}>
+                            <td>{score.MatchDate}</td>
+                            <td>{score.Player1Score}</td>
+                            <td>{score.Player2Score}</td>
+                            <td>{score.Player3Score}</td>
+                            <td>{score.Player4Score}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </div>
+
+                ))}
+              </div>
+            )}
           </div>
         </Col>
       </Row>
